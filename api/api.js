d765351eb17edfeb2ac3ba825d00a4946f702844
Carlos Silvestri <54336195+carlossilvestri@users.noreply.github.com>
2021-03-27 21:27:30 -0400

    Update api.js

--- api/api.js
+++ api/api.js
@@ -68,7 +68,7 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 
-server.listen(port, host, () => {
+app.listen(port, host, () => {
   if (environment !== 'production' &&
     environment !== 'development' &&
     environment !== 'testing'
